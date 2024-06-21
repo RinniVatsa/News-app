7d62438406dbe090e4ede00760b11fd76d3ee3ac
Rinni Vatsa <rinnivatsa09@gmail.com>
2024-06-21 14:12:48 +0530

    Search Feature commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/ store';
 import './index.css';
 
-ReactDOM.render( <
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render( <
     Provider store = { store } >
     <
     App / >
     <
-    /Provider>,
-    document.getElementById('root')
+    /Provider>
 );
